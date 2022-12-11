59e5faf0b0e37455f3e1e1a3728a312b8a94c302
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-12-11 17:54:08 +0100

    day 3 pt2

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,20 @@ const FILE_PATH: &str = "src/input.txt";
 fn main() {
   let file_contents = fs::read_to_string(FILE_PATH).unwrap();
   let mut sum = 0;
-  let mut cur_chars: Vec<char> = vec![];
+  let mut count = 0;
+  let mut cur_lines: Vec<&str> = vec![];
+  let mut cur_char: char = ' ';
 
   for l in file_contents.lines() {
-    let mid: usize = l.len() / 2;
-    let (s1, s2) = l.split_at(mid);
+    if count < 3 {
+      cur_lines.push(l);
+      count += 1;
+      continue;
+    }
 
-    for c in s2.chars() {
-      if s1.contains(c) && !cur_chars.contains(&c) {
-        cur_chars.push(c);
-        // this way we only need to check the char once and can just add the offset to the result
+    for c in cur_lines.first().unwrap().chars() {
+      if cur_lines.get(1).unwrap().contains(c) && cur_lines.get(2).unwrap().contains(c) && c != cur_char {
+        cur_char = c;
         if c.is_uppercase() {
           sum += 26;
         }
@@ -24,8 +28,11 @@ fn main() {
         sum += c as u16 - 96;
       }
     }
-    cur_chars = vec![];
+
+    cur_lines = vec![l];
+    count = 1;
+    cur_char = ' ';
   }
 
-  println!("====Answer====\n{}", sum);
+  println!("\n====Answer====\n{}", sum);
 }
