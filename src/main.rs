4f42cffc7c0fbf5968e3dff076407badc96b936f
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-12-13 16:00:54 +0100

    day 6 pt1

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,21 @@ const FILE_PATH: &str = "src/input.txt";
 fn main() {
   let file_contents = fs::read_to_string(FILE_PATH).unwrap();
   let mut sum = 0;
+  let mut chars_vec: Vec<char> = vec![];
 
   for l in file_contents.lines() {
-    let l_vec: Vec<&str> = l.split(',').collect();
-    let (part1, part2) = (l_vec.first().unwrap().to_owned(), l_vec.last().unwrap().to_owned());
-    let part1_nums: Vec<&str> = part1.split('-').collect();
-    let part2_nums: Vec<&str> = part2.split('-').collect();
-    let (part1_lower, part1_higher) = (
-      part1_nums.first().unwrap().parse::<i32>().unwrap(),
-      part1_nums.last().unwrap().parse::<i32>().unwrap(),
-    );
-    let (part2_lower, part2_higher) = (
-      part2_nums.first().unwrap().parse::<i32>().unwrap(),
-      part2_nums.last().unwrap().parse::<i32>().unwrap(),
-    );
-
-    if (part1_lower <= part2_lower && part1_higher >= part2_higher)
-      || (part2_lower <= part1_lower && part2_higher >= part1_higher)
-      || (part1_higher == part2_lower)
-      || (part1_higher <= part2_higher && part1_lower <= part2_higher && part1_higher >= part2_lower)
-      || (part2_higher <= part1_higher && part2_lower <= part1_higher && part2_higher >= part1_lower)
-    {
-      print!("{part1_lower}, {part1_higher} -- ");
-      println!("{part2_lower}, {part2_higher}");
-      sum += 1;
+    while chars_vec.len() < 4 {
+      let mut iter = l.chars();
+      if sum > 0 {
+        for _ in 0..sum {
+          iter.next();
+        }
+      }
+      chars_vec = iter.take(4).collect();
+      println!("{:?}", chars_vec);
+      chars_vec.sort_unstable();
+      chars_vec.dedup();
+      sum += 4;
     }
   }
 
